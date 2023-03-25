d92a042784e3f9fac94c7fb32716133b6fdc00c1
Igo Santana <igo.santana33@gmail.com>
2023-03-25 16:47:50 -0300

    hotfix: .env files

--- src/common/services/api.ts
+++ src/common/services/api.ts
@@ -1,18 +1,22 @@
-import axios from 'axios'
+import axios from "axios";
 import { ComicsResData } from "../interfaces/comicRes.interface";
-import { OneComicReqRes } from '../interfaces/oneComicRes.interface';
+import { OneComicReqRes } from "../interfaces/oneComicRes.interface";
 
 const getComics = async (offSet = 0): Promise<ComicsResData> => {
-    const response = await axios.get<ComicsResData>(`http://gateway.marvel.com/v1/public/comics?ts=1&apikey=bccc63563d81110837fe6e2469dd9e48&hash=a2c9c6a9f035c18324ec13e8caf6d30c&format=comic&formatType=comic&dateDescriptor=thisMonth&limit=10&offset=${offSet}`)
-    return response.data
-}
+  const response = await axios.get<ComicsResData>(
+    `https://gateway.marvel.com/v1/public/comics?ts=1&apikey=${process.env.NEXT_PUBLIC_API_KEY}&hash=${process.env.NEXT_PUBLIC_HASH_KEY}&format=comic&formatType=comic&dateDescriptor=thisMonth&limit=10&offset=${offSet}`
+  );
+  return response.data;
+};
 
 const getOneComic = async (comicId: string): Promise<OneComicReqRes> => {
-    const response = await axios.get<OneComicReqRes>(`http://gateway.marvel.com/v1/public/comics/${comicId}?ts=1&apikey=bccc63563d81110837fe6e2469dd9e48&hash=a2c9c6a9f035c18324ec13e8caf6d30c`)
-    return response.data
-}
+  const response = await axios.get<OneComicReqRes>(
+    `https://gateway.marvel.com/v1/public/comics/${comicId}?ts=1&apikey${process.env.NEXT_PUBLIC_API_KEY}&hash=${process.env.NEXT_PUBLIC_HASH_KEY}`
+  );
+  return response.data;
+};
 
 export const api = {
-    getComics,
-    getOneComic
-}
+  getComics,
+  getOneComic,
+};
